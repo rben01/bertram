4d0e06e428a9261a11b39e1d0dd1dcb2247aa272
Robert Bennett <rltbennett@icloud.com>
2022-12-31 14:21:46 -0500

    Added icon (such as it is)

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+version="$1"
 (
 	code --uninstall-extension 'rltb.bertram' || :
-	cd gen_theme && python split_themes.py && cd .. && vsce package && code --install-extension bertram-0.5.0.vsix
+	cd gen_theme && python split_themes.py && cd .. && vsce package && code --install-extension bertram-"$version".vsix
 )
